49d4263f6ce520ec500c00897fa4b64e4f062cfc
jesscura <jesel@sagestone.page>
2026-03-03 07:56:40 +0800

    Add website app and deploy frontend services to Railway

---
+++ apps/website/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Getswyft Website</h1>
+      <p className="read-the-docs">Marketing site is running on Railway.</p>
+    </>
+  )
+}
+
+export default App
